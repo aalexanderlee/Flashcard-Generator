b2c121e22e4042149288e770da55568b07e7686e
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2017-07-16 17:38:31 -0700

    created a basic key object to call from and reconfiguring loop for all keys within our new basicCard object for prompting

--- BasicCard.js
+++ BasicCard.js
@@ -1,10 +1,12 @@
 //******** tools for basic card ********//
+var keys = require("./keys.js"); //grabs objects from keys.js with my saved answers for comparison
+var basicKeyList = keys.basicKeys; //grabs values from twitterKeys object
 
-//all this shit in BasicCard has empty values for now, fill with "new"
+//all this shit in BasicCard has empty values for now, fill with "new" in qObj
 function BasicCard(trigger, front, back) {
 	this.trigger = true;
-	this.front = message; //my questions
-	this.answer = type; //user answers; compare this shit to the stored answers in a json package 
+	this.front = front; //my questions via prompt "message"
+	this.answer = back; //user answer input
 	this.executeThis = function() {
 		if (this.trigger) {
 			//execute prompting functions below
@@ -17,23 +19,24 @@ function BasicCard(trigger, front, back) {
 
 //qObj contains arguments we will insert to BasicCard for each question instance
 var qObj = {};
-qObj.qOne = new BasicCard (true, "How many bagels does it take to make a Jerome?", 40); //trigger, this.front, this.back
+qObj.qOne = new BasicCard (true, "How many bagels does it take to make a Jerome?", inquirer); //trigger, this.front, this.back
 qObj.qTwo = new BasicCard (true, "What are pushed to project repos on Github?", "puppies"); //trigger, this.front, this.back
 qObj.qThree = new BasicCard (true, "What type of animal is Farley?", "cat"); //trigger, this.front, this.back
 qObj.qFour = new BasicCard (true, "How was Tindoor rated?", "most useful"); //trigger, this.front, this.back
 qObj.qFive = new BasicCard (true, "Are you a cheeky mofo?", "yes"); //trigger, this.front, this.back
 
+for (var key in qObj) {
+	var allQuestions = function(qOne, qTwo, qThree, qFour, qFive) {
+		if (this.trigger) {
+			inquirer.prompt([
+				{
+				 type: "input";
+				 name: "question";
+				 message: BasicCard.front; //this.back = 40 
+				},
 
-var allQuestions = function(qOne, qTwo, qThree, qFour, qFive) {
-	if (this.trigger) {
-		inquirer.prompt([
-			{
-			 type: "input";
-			 name: "question";
-			 message: BasicCard.front; //this.back = 40 
-			},
-
-			])
+				])
+		}
 	}
 }
 
---
+++ keys.js
@@ -0,0 +1,15 @@
+//.gitignore probably not needed, nothing confidential here 
+
+//export keys, similar to LIRI keys and values
+exports.basicKeys = {
+	aOne: "40",
+	aTwo: "puppies",
+	aThree: "cat",
+	aFour: "most useful",
+	aFive: "yes"
+}
+
+exports.clozeKeys = {
+	// client_id: '6f4c173a54414b55bc85d37094b217ac',
+	// client_secret: '4124cb28474b4741ae2eea3b36138e44'
+}
