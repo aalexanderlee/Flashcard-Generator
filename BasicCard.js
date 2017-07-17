045f6c134f3bed35718c4c03ba87db010f95ea54
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2017-07-16 17:24:22 -0700

    adding suggestions for future functions managing prompt feature and input gathering

--- BasicCard.js
+++ BasicCard.js
@@ -8,17 +8,20 @@ function BasicCard(trigger, front, back) {
 	this.executeThis = function() {
 		if (this.trigger) {
 			//execute prompting functions below
-
+			//this function should be able to gather their input from inquirer
+			//this function should compare user input with correct response.answers we have stored in a json package
+			//the json package should probably be in an adjacent file
 		}
 	};
 }
 
+//qObj contains arguments we will insert to BasicCard for each question instance
 var qObj = {};
-qObj.qOne = new BasicCard (true, "How many bagels does it take to make a Jerome?", 40);
-qObj.qTwo = new BasicCard (true, "What are pushed to project repos on Github?", "puppies");
-qObj.qThree = new BasicCard (true, "What type of animal is Farley?", "cat");
-qObj.qFour = new BasicCard (true, "How was Tindoor rated?", "most useful");
-qObj.qFive = new BasicCard (true, "Are you a cheeky mofo?", "yes");
+qObj.qOne = new BasicCard (true, "How many bagels does it take to make a Jerome?", 40); //trigger, this.front, this.back
+qObj.qTwo = new BasicCard (true, "What are pushed to project repos on Github?", "puppies"); //trigger, this.front, this.back
+qObj.qThree = new BasicCard (true, "What type of animal is Farley?", "cat"); //trigger, this.front, this.back
+qObj.qFour = new BasicCard (true, "How was Tindoor rated?", "most useful"); //trigger, this.front, this.back
+qObj.qFive = new BasicCard (true, "Are you a cheeky mofo?", "yes"); //trigger, this.front, this.back
 
 
 var allQuestions = function(qOne, qTwo, qThree, qFour, qFive) {
