815ef98209474113b01d5407b37a317c4f4f63ac
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2017-07-17 18:07:48 -0700

    make a play again maybe function

--- BasicCard.js
+++ BasicCard.js
@@ -1,71 +1,65 @@
 //******** tools for basic card ********//
 var keys = require("./keys.js"); //grabs objects from keys.js with my saved answers for comparison
-//var basicKeyList = keys.basicKeys; //grabs values from basicKeys object
 
-//note that to get 100% correct, user has to satisfy these conditions as it iterates through qObj:
-//guess.userGuess = basicKeyList.aOne;
-//guess.userGuess = basicKeyList.aTwo;
-//guess.userGuess = basicKeyList.aThree;
-//guess.userGuess = basicKeyList.aFour;
-//guess.userGuess = basicKeyList.aFive;
 
 var score = 0; //start scoring at zero
-console.log("Your starting score is " + score + "."); //display this score
+console.log("Your initial score is " + score + "."); //display initial score
 
-//all this shit in BasicCard has empty values for now, fill with "new" in qObj
+//all this in BasicCard constructor has empty values for now, fill with "new" in qObj
 function BasicCard(trigger, front, back) {
 	this.trigger = true;
-	this.front = front; //my questions via prompt "message"
-	this.back = back; //user answer input
+	this.front = front; //inquirer "message" is our question front
+	this.back = back; //stores correct answers
 	this.executeThis = function() {
 		if (this.trigger) {
-			//allQuestions(); ---> this can change, but for now this makes sense
-			//execute prompting functions below
-			//this function should be able to gather their input from inquirer
-			//this function should compare user input with correct response.answers we have stored in a json package
-			//the json package should probably be in an adjacent file
+			//allQuestions(); ---> wrap this function around everything.
 		}
 	};
 }
 
-//qObj contains arguments we will insert to BasicCard for each question instance
-//prompt user with your message aka question
-//as alternative way, compare the input value to my actual answer keys in keys.js in keys.basicKeys object list 
+//qObj{} contains all of our saved data (questions & correct answers)
+//trigger = true || false; allows the switching on or off of certain questions later if needed
 var qObj = {};
-qObj.qOne = new BasicCard (true, "How many bagels does it take to make a Jerome?", "40 bagels"); //trigger, this.front, this.back
-qObj.qTwo = new BasicCard (true, "What are pushed to project repos on Github?", "puppies"); //trigger, this.front, this.back
-qObj.qThree = new BasicCard (true, "What type of animal is Farley?", "cat"); //trigger, this.front, this.back
-qObj.qFour = new BasicCard (true, "How was Tindoor rated?", "most useful"); //trigger, this.front, this.back
-qObj.qFive = new BasicCard (true, "Are you a cheeky mofo?", "yes"); //trigger, this.front, this.back
+qObj.qOne = new BasicCard (true, "How many bagels does it take to make a Jerome?", "40 bagels"); 
+qObj.qTwo = new BasicCard (true, "What are pushed to project repos on Github?", "puppies"); 
+qObj.qThree = new BasicCard (true, "What type of animal is Farley?", "cat"); 
+qObj.qFour = new BasicCard (true, "How was Tindoor rated?", "most useful"); 
+qObj.qFive = new BasicCard (true, "Are you a cheeky mofo?", "yes"); 
 
+//this function will go through qObj and compare inputs as guess.userGuess
 var allQuestions = function() {
 	for (var key in qObj) {   	
-		if (this.trigger) {			
+		
+		if (this.trigger && score < 5) {		
+
 			inquirer.prompt([
 
 				{
-				 type: "input", //this.back will be collected as an arbitrary string value the user types
-				 name: "userGuess", //guess.userGuess will store this.back string values
-				 message: BasicCard.front //this will be the question user sees from this.front string value
+				 type: "input", //user puts in some string type
+				 name: "userGuess", //guess.userGuess will be the user string input value
+				 message: BasicCard.front //displays the question as we go through qObj
 				}			
 					
 				]).then(function(guess) {
 						if (guess.userGuess === BasicCard.back){
-							score++
-							console.log("That is correct!");
-							console.log("Your new score is " + score + ".");
+							score++ //make the score go up one if it's a match
+							console.log("That is correct!"); //tell the user it's correct
+							console.log("Your new score is " + score + "."); //display the score increment
 						} else {
-							console.log("Maybe better luck next time.");
+							console.log("Maybe better luck next time."); 
+							//the user guess is wrong
+							//continue prompting through qObj
 						}
 
-					});
+				});
 					
-				}
-		
 		}
+		
 	}
 
-}; 
+	playAgainMaybe();
+}
+
 
 if (score === 5) {
 	console.log("Here is your final score: " + score + ".");
