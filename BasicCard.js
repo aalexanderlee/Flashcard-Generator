44be65af389b5b1a678341b5a00addac77e38c80
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2017-07-17 18:52:04 -0700

    attempting to wrap functions in cloze and basic for main.js

--- BasicCard.js
+++ BasicCard.js
@@ -1,25 +1,25 @@
 //******** tools for basic card ********//
-var keys = require("./keys.js"); //grabs objects from keys.js with my saved answers for comparison
+
+var fs = require("fs"); // only if we want to log/append our questions and results later to a text file
+
+var score = 0; // start scoring at zero
+console.log("Your initial score is " + score + "."); // display initial score
 
 
-var score = 0; //start scoring at zero
-console.log("Your initial score is " + score + "."); //display initial score
-
-
-//all this in BasicCard constructor has empty values for now, fill with "new" in qObj
+// all this in BasicCard constructor has empty values for now, fill with "new" in qObj
 function BasicCard(trigger, front, back) {
-	this.trigger = true; //trigger = true || false; allows the switching on or off of questions
-	this.front = front; //inquirer "message" is our question front
-	this.back = back; //stores correct answers
+	this.trigger = true; // trigger = true || false; allows the switching on or off of questions
+	this.front = front; // inquirer "message" is our question front
+	this.back = back; // stores correct answers
 	this.executeThis = function() {
 		if (this.trigger) {
-			//allQuestions(); ---> wrap this function around everything. Where should this wrap?
+			// allQuestions(); ---> wrap this function around everything. Where should this wrap?
 		}
 	};
 }
 
 
-//qObj{} contains all of our saved data (questions & correct answers)
+// qObj{} contains all of our saved data (questions & correct answers)
 var qObj = {};
 qObj.qOne = new BasicCard (true, "How many bagels does it take to make a Jerome?", "40 bagels"); 
 qObj.qTwo = new BasicCard (true, "What are pushed to project repos on Github?", "puppies"); 
@@ -28,7 +28,7 @@ qObj.qFour = new BasicCard (true, "How was Tindoor rated?", "most useful");
 qObj.qFive = new BasicCard (true, "Are you a cheeky mofo?", "yes"); 
 
 
-//this function will go through qObj and compare inputs as guess.userGuess
+// this function will go through qObj and compare inputs as guess.userGuess
 var allQuestions = function() {
 	for (var key in qObj) {   	
 		
@@ -37,20 +37,20 @@ var allQuestions = function() {
 			inquirer.prompt([
 
 				{
-				 type: "input", //user puts in some string type
-				 name: "userGuess", //guess.userGuess will be the user string input value
-				 message: BasicCard.front //displays the question as we go through qObj
+				 type: "input", // user puts in some string type
+				 name: "userGuess", // guess.userGuess will be the user string input value
+				 message: BasicCard.front // displays the question as we go through qObj
 				}			
 					
 			]).then(function(guess) {
 					if (guess.userGuess === BasicCard.back){
-						score++ //make the score go up one if it's a match
-						console.log("That is correct!"); //tell the user it's correct
-						console.log("Your new score is " + score + "."); //display the score increment
+						score++ // make the score go up one if it's a match
+						console.log("That is correct!"); // tell the user it's correct
+						console.log("Your new score is " + score + "."); // display the score increment
 					} else {
 						console.log("Maybe better luck next time."); 
-						//the user guess is wrong
-						//continue prompting through qObj
+						// the user guess is wrong
+						// continue prompting through qObj
 					}
 
 			});
@@ -68,18 +68,18 @@ var playAgainMaybe = function() {
 	inquirer.prompt ([
 				
 		{
-		 type: "list", //the input type will be a list of "yes" or "no"
-		 name: userDecision, //stores the user input of "yes" or "no"
-		 message: "Would you like to play again?", //will display 
+		 type: "list", // the input type will be a list of "yes" or "no"
+		 name: userDecision, // stores the user input of "yes" or "no"
+		 message: "Would you like to play again?", // will display 
 		 choices: ["Yes", "No"]
 		}
 
 	]).then(function(answer) {
-		//if yes, reset score and play again
+		// if yes, reset score and play again
 		if (answer.userDecision === "Yes") {
 			score = 0;
 			allQuestions();
-		//if no, reset score and exit completely
+		// if no, reset score and exit completely
 		} else if (answer.userDecision === "No") {
 			score = 0;
 			process.exit()
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,31 +1,31 @@
 //******** tools for cloze card *********//
-var keys = require("./keys.js"); //grabs objects from keys.js with my saved answers for comparison
+
+var fs = require("fs"); // only if we want to log/append our questions and results later to a text file
+
+var score = 0; // initial score
+console.log("Your initial score is " + score + "."); // display initial score
 
 
-var score = 0; //initial score
-console.log("Your initial score is " + score + "."); //display initial score
-
-
+//all this in ClozeCard constructor has empty values for now, fill with "new" in clozeObj
 function ClozeCard(trigger, cloze, partial, fullText) {
-	//Options for doing this:
-	//1) fullText exist first and we split(), then put into "message aka this.partial" and "cloze" if "cloze" is correct
-	//2) provide "partial" as "message", grab "input", compare to "cloze" then join() and console.log if "cloze" matches our key
-	//concatinate or split()/join()?
+	// Options for doing this:
+	// 1) fullText exist first and we split(), then put into "message aka this.partial" and "cloze" if "cloze" is correct
+	// 2) provide "partial" as "message", grab "input", compare to "cloze" then join() and console.log if "cloze" matches our key
+	// concatinate or split()/join()?
 
-	this.trigger = true; //switches the program on
-	this.cloze = cloze; //this is the missing answer
-    this.partial = partial; //this is the shown part with "..." in cloze position or index
-	this.fullText = fullText; //this will exist after you stick cloze and partial together
+	this.trigger = true; // switches the program on
+	this.cloze = cloze; // this is the missing answer
+    this.partial = partial; // this is the shown part with "..." in cloze position or index
+	this.fullText = fullText; // this will exist after you stick cloze and partial together
 	this.executeThese = function(){
 		if (this.trigger) {
-			//allClozeQuestions(); ---> wrap this function around everything. Where should this wrap?
+			// allClozeQuestions(); ---> wrap this function around everything. Where should this wrap?
 		}
-	}
-
+	};
 }
 
 var clozeObj = {};
-//note: clozeObj.number = new ClozeCard (trigger, this.cloze, this.partial, this.fullText)
+// note: clozeObj.number = new ClozeCard (trigger, this.cloze, this.partial, this.fullText)
 clozeObj.one = new ClozeCard (true, "40 bagels", "...make a Jerome", "40 bagels make a Jerome");
 clozeObj.two = new ClozeCard (true, "Push all puppies", "...to Github", "Push all puppies to Github");
 clozeObj.three = new ClozeCard (true, "a cat", "Farley is ...", "Farley is a cat");
@@ -41,24 +41,24 @@ var allClozeQuestions = function() {
 			inquirer.prompt([
 
 				{
-				 type: "input", //user puts in some string type
-				 name: "userCloze", //guess.userCloze will be compared to this.cloze
-				 message: ClozeCard.partial //displays "...blah blah blah"
+				 type: "input", // user puts in some string type
+				 name: "userCloze", // guess.userCloze will be compared to this.cloze
+				 message: ClozeCard.partial // displays "...blah blah blah"
 				}			
 					
 			]).then(function(guess) {
 					if (guess.userCloze === ClozeCard.partial) {
-						score++ //make the score go up one if it's a match
-						console.log("That is correct!"); //tell the user it's correct
+						score++ // make the score go up one if it's a match
+						console.log("That is correct!"); // tell the user it's correct
 
-						//put cloze and partial into an array and join at the CORRECT indices
+						// put cloze and partial into an array and join at the CORRECT indices
 
-						console.log("Your new score is " + score + "."); //display the score increment
+						console.log("Your new score is " + score + "."); // display the score increment
 
 					} else {
 						console.log("Maybe better luck next time."); 
-						//the user guess is wrong
-						//continue prompting through qObj
+						// the user guess is wrong
+						// continue prompting through qObj
 					}
 
 			});
@@ -75,18 +75,18 @@ var playAgainMaybe = function() {
 	inquirer.prompt ([
 				
 		{
-		 type: "list", //the input type will be a list of "yes" or "no"
-		 name: userDecision, //stores the user input of "yes" or "no"
-		 message: "Would you like to play again?", //will display 
+		 type: "list", // the input type will be a list of "yes" or "no"
+		 name: userDecision, // stores the user input of "yes" or "no"
+		 message: "Would you like to play again?", // will display 
 		 choices: ["Yes", "No"]
 		}
 
 	]).then(function(answer) {
-		//if yes, reset score and play again
+		// if yes, reset score and play again
 		if (answer.userDecision === "Yes") {
 			score = 0;
 			allQuestions();
-		//if no, reset score and exit completely
+		// if no, reset score and exit completely
 		} else if (answer.userDecision === "No") {
 			score = 0;
 			process.exit()
