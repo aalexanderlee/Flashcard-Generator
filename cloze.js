a8e4da54606c5f72018769e258205be4af3fd8fc
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2017-07-25 23:46:24 -0700

    cloze.js will follow the same constructor format as basic.js with an added this.clozeDeleted method for this cloze card types

--- cloze.js
+++ cloze.js
@@ -12,7 +12,16 @@ function ClozeFlashCard(text, cloze) {
 	this.closeDeleted = this.text.replace(this.cloze, "...");
 	this.create = function() {
 		//create() will return the data object for cloze card
-		var data
-	}
+		var data = {
+			text: this.text,
+			cloze: this.cloze,
+			clozeDeleted: this.clozeDeleted,
+			type: "cloze"
+		};
+
+		fs.appendFile("log.txt", JSON.stringify(data) + ";", "utf8", function(error) {
+			if (error) throw error;
+		});
+	};
 
 }
